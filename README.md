5cae6e1a3e04c3c95fdbe5b9374bed9bf4328ed5
gopikishan001 <gkay140@gmail.com>
2022-06-22 19:48:56 +0530

    update

--- README.md
+++ README.md
@@ -4,13 +4,10 @@ A new Flutter project.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+This project is about a medicine reminder application made in flutter.
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+where you can :
+    - Track inventory of medicine.
+    - Mark and schedule all the medicines needed to take.
+    - Get udates of which medicine need to take today.
+    - notification and alarm for medicine timings (feature not added yet).
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,72 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter/widgets.dart';
 
-class calender extends StatefulWidget {
-  const calender({Key? key}) : super(key: key);
-
-  @override
-  State<calender> createState() => _calenderState();
-}
-
-class _calenderState extends State<calender> {
-  List<DateTime> mySelectedDates = [];
-  DateTime selectedCalDate = DateTime.now();
-  // Map<DateTime, List> listEvents = {};
-
-  // eventLoder(day) {
-  //   return listEvents[day];
-  // }
+class gk extends StatelessWidget {
+  const gk({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          TableCalendar(
-            firstDay: DateTime(2000, 1, 1),
-            lastDay: DateTime(2030, 12, 31),
-            focusedDay: selectedCalDate,
-
-            // eventLoader: eventLoder(selectedCalDate),
-
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                selectedCalDate = selectedDay;
-
-                if (mySelectedDates.contains(selectedCalDate)) {
-                  mySelectedDates.remove(selectedCalDate);
-                  // listEvents.remove(selectedCalDate);
-                } else {
-                  mySelectedDates.add(selectedCalDate);
-                  // listEvents[selectedCalDate] = ["Event"];
-                }
-              });
-            },
-            // calendarStyle: const CalendarStyle(
-            //   selectedDecoration: BoxDecoration(
-            //     color: Color.fromARGB(219, 162, 255, 205),
-            //     shape: BoxShape.circle,
-            //   ),
-            // ),
-          ),
-          const SizedBox(height: 40),
-          Expanded(
-            child: ListView.builder(
-              itemCount: mySelectedDates.length,
-              itemBuilder: (BuildContext context, index) {
-                return Container(
-                  padding: const EdgeInsets.all(5),
-                  child: Text(
-                    mySelectedDates[mySelectedDates.length - 1 - index]
-                        .toString(),
-                    textScaleFactor: 1.5,
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+    return const Text("data");
   }
 }
